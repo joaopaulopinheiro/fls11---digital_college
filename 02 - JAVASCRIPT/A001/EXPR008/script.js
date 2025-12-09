5f8a9658a72f511af5fc2d56212dac36f2cf76c3
João Paulo Pinheiro <pinheiro.contatojoao@gmail.com>
2025-12-09 19:25:01 -0300

    Add EXPR007 & EXPR008

---
+++ 02 - JAVASCRIPT/A001/EXPR008/script.js
@@ -0,0 +1,24 @@
+/* EXPR008 - SOLICITE AO USUARIO OS DADOS: 
+ 
+NOME, CURSO, SEDE, TURMA, PROFESSOR
+
+IMPRIMIR:
+
+Aluno: seu nome
+Curso: nome do curso
+Sede: nome da sede
+Turma: noma da turma
+Professor: David Wanderson */
+
+console.log('==== IMPRIMIR DADOS ====')
+
+let aluno = prompt('Digite seu nome: ')
+let curso = prompt('Digite seu curso: ')
+let sede = prompt('Digite sua sede: ')
+let turma = prompt('Digite sua turma: ')
+
+console.log('\nAluno: ' + aluno)
+console.log('Curso: ' + curso)
+console.log('Sede: ' + sede)
+console.log('Turma: ' + turma)
+console.log('Professor: David Wanderson')
